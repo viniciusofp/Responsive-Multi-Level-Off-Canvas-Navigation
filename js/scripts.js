c6734abf3a3b074017f59e9ed0a5a2ee12e8e078
viniciusofp <viniciusofp@gmail.com>
2017-03-26 13:10:41 -0300

    v1.0.0

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$('.sections-trigger').click(function() {
+	$('#sections-nav').toggleClass('navOpen');
+	$('#wrapper').toggleClass('navOpen');
+	$('#nav-inner').toggleClass('navOpen');
+});
+
+
+$('#search-btn').click(function() {
+	$(this).toggleClass('closed').toggleClass('opened');
+	$('#search-field').toggleClass('closed').toggleClass('opened');
+});
+
+$( ".section-menu > li").hover(
+  function(e) {
+    $( this ).addClass( "hover" );
+  }, function() {
+    $( this ).removeClass( "hover" );
+  }
+ );
+
+$( ".section-menu li a").hover(
+  function(e) {
+    $( this ).addClass( "hover" );
+    $( this ).parent('.section-menu > li').addClass( "unhover" );
+  }, function() {
+    $( this ).removeClass( "hover" );
+    $( this ).parent('.section-menu > li').removeClass( "unhover" );
+  }
+ );
+
+$( ".section-menu > li").click(function() {
+	console.log('oi');
+	$(this).children(".drop-menu").toggleClass('opened');
+});
+
+
